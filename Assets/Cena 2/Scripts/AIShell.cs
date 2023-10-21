b2b12bfe24f672a55df0f428337f8fcb94abe09b
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-21 19:13:21 +0100

    Bullets Face Right Direction

--- Assets/Cena 2/Scripts/AIShell.cs
+++ Assets/Cena 2/Scripts/AIShell.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AIShell : MonoBehaviour
 {
     public GameObject explosion;
-
+    Rigidbody rb;
 
     void OnCollisionEnter(Collision col)
     {
@@ -21,12 +21,12 @@ public class AIShell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.forward = rb.velocity;
     }
 }
